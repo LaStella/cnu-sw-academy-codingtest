7a7e0c619611d8b7138f4a5598442727a0625ef6
LaStella <jingyu.ham94@gmail.com>
2023-01-11 09:34:24 +0900

    1월 10일 [하] Umm 문자열 1 수정 주어진 문자열의 a~b범위 내에 Umm문자열이 존재하면 되는 것으로 문제를 해석하여 발생한 오류를 해결하였습니다.

--- month1/_10_Umm1.java
+++ month1/_10_Umm1.java
@@ -10,16 +10,27 @@ public class _10_Umm1 {
             String s = sc.next();
             int a = sc.nextInt()-1;
             int b = sc.nextInt();
+            boolean isUmm = false;
             
-            s = s.substring(a, b);
-            if (s.contains("Umm")) {
-                if(s.contains("UmmUmm")) {
-                    System.out.println("NO");
-                    continue;
+            // 첫글자가 U인지 확인합니다.
+            if (s.charAt(a) == 'U') {
+                int count = 0;
+                for (int i = a+1 ; i < b ; i++) {
+                    // 다른 U가 존재하면 Umm문자열이 아닙니다.
+                    if (s.charAt(i) == 'U') {
+                        isUmm = false;
+                        break;
+                    }
+                    // U가아닌 문자는 m뿐이므로 m의 개수를 증가시키며, 2개 이상인 경우 Umm문자열입니다.
+                    if (++count > 1) isUmm = true;
                 }
+            }
+            
+            if (isUmm) {
                 System.out.println("YES");
                 continue;
             }
+            
             System.out.println("NO");
         }
     }
