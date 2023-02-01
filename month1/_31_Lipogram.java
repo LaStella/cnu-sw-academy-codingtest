accb495452abb5f85b7cbb59c4314abaf37f812d
Jingyu Ham <jingyu.ham94@gmail.com>
2023-02-02 00:14:19 +0900

    1월 31일 [하] 리포그램 문제 수정 저장을 안하고 커밋하여 수정 후 커밋

--- month1/_31_Lipogram.java
+++ month1/_31_Lipogram.java
@@ -2,17 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Arrays.stream(br.readLine().split(""))
-                .map(String::trim)
+        String check = "abcdefghijklmnopqrstuvwxyz";
+        Set<String> set = Arrays.stream(br.readLine().split(""))
+                .filter(s -> !s.trim().isEmpty())
                 .map(String::toLowerCase)
-                .distinct()
-                .collect(Collectors.joining())
-                .length();
-        System.out.println(count == 26 ? "YES" : "NO");
+                .collect(Collectors.toSet());
+        if (set.size() == 26) {
+            System.out.println("NO");
+            return;
+        }
+        System.out.println("YES");
+        StringBuilder sb = new StringBuilder();
+        for (String s : check.split("")) {
+            if (!set.contains(s)) sb.append(s);
+        }
+        System.out.println(sb);
     }
 }
