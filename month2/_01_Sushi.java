d57f3f736680181b148d518d2a5f60b4e682eab5
Jingyu Ham <jingyu.ham94@gmail.com>
2023-02-03 00:04:00 +0900

    2월 1일 [B1] 320개 문제 수정 저장을 안하고 커밋하여 수정 후 커밋

--- month2/_01_Sushi.java
+++ month2/_01_Sushi.java
@@ -2,17 +2,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Arrays.stream(br.readLine().split(""))
-                .map(String::trim)
-                .map(String::toLowerCase)
-                .distinct()
-                .collect(Collectors.joining())
-                .length();
-        System.out.println(count == 26 ? "YES" : "NO");
+        int size = Integer.parseInt(br.readLine());
+        int[] sushi = Arrays.stream(br.readLine().split(" ")).mapToInt(s -> Math.abs(Integer.parseInt(s)-320)).toArray();
+        int minIndex = IntStream.range(0, size).reduce((i, j) -> sushi[i] <= sushi[j] ? i : j).getAsInt();
+
+        System.out.println(minIndex+1);
     }
 }
