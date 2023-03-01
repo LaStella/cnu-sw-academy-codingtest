188382a45bddd0756e917701a8d924616f3d577d
Jingyu Ham <jingyu.ham94@gmail.com>
2023-03-01 22:36:08 +0900

    2월 23일 [중] [그래프] 정점의 차수 문제 추가

---
+++ month2/_23_Degree.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int vertexSize = Integer.parseInt(st.nextToken());
+        int edgeSize = Integer.parseInt(st.nextToken());
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i <= vertexSize; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        while (edgeSize-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int pointU = Integer.parseInt(st.nextToken());
+            int pointV = Integer.parseInt(st.nextToken());
+
+            adjList.get(pointU).add(pointV);
+            adjList.get(pointV).add(pointU);
+        }
+
+        int result = 0;
+        for (int i = 1; i <= vertexSize; i++) {
+            result += adjList.get(i).size();
+        }
+
+        System.out.println(result);
+    }
+}
